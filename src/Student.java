1e5f14aafb1225501c15f4a2ba9e2f8d616ddb78
Adam <Adam@pl-00107668.uni.mdx.ac.uk>
2014-11-03 16:33:50 +0000

    New Student class

---
+++ src/Student.java
@@ -0,0 +1,16 @@
+
+public class Student {
+
+	
+	private String name;
+	
+	public Student(String name){
+		
+		this.name = name;
+	}
+	
+	public void print(){
+		
+		System.out.print("MyName: " + name);
+	}
+}
